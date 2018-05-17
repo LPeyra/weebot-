4a46cf917a126fda352e37360f584538374e57f3
LPeyra <39248512+LPeyra@users.noreply.github.com>
2018-05-17 19:05:01 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,24 @@
+
+// Log our bot in
+client.login(token);
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
+
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.channel.send('PONG!');
+  	}
+});
+
+client.on('message', message => {
+    if (message.content === 'bing') {
+    	message.reply('BONG!');
+  	}
+});
+
+
+client.login(process.env.BOT_TOKEN);
