dd92022d7d6d0057c80742d340848544b779fbe0
LPeyra <39248512+LPeyra@users.noreply.github.com>
2018-05-29 15:43:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const { PREFIX } = require('./config');
 const fs = require('fs');
-const ytdl = require('ytdl-core');
+//const ytdl = require('ytdl-core');
 const streamOptions = { seek: 0, volume: 1 };
 const broadcast = client.createVoiceBroadcast();
 
